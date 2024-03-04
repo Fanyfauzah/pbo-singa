d178afc0243d6e1c68532bdc202db03b6ec379c6
fanyfauzah <fanyfauzahyunansyah@gmail.com>
2024-03-04 15:26:13 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+        public static void main(String[] args) {
+            singa lion=new singa();
+            lion.Nama();
+            lion.Makanan();
+            lion.warna();
+            lion.umur();
+            lion.berattubuh();
+        }
+
+
+
+
+
+
+
+
+    }
+
---
+++ src/singa.java
@@ -0,0 +1,30 @@
+public class singa{
+        String Nama = "Haruto";
+        String warna = "Oren";
+        String Makanan = "Daging";
+        int umur = 2 ;
+        int berattubuh = 4;
+
+
+        public void Nama() {
+            System.out.println("Nama singa"+ this.Nama );
+        }
+
+        public void warna() {
+
+            System.out.println(this.Nama+"berwarna" + this.warna);
+        }
+        public void Makanan() {
+            System.out.println(this.Nama + "Makan" + this.Makanan);
+        }
+
+        public void umur(){
+            System.out.println(this.Nama+"berumur"+this.umur);
+        }
+
+        public void berattubuh(){
+            System.out.println(this.Nama+"berat tubuhnya"+this.berattubuh);
+        }
+
+
+    }
